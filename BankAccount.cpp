940bd5ffe5fd19c40b0559215d1f6f78bc57130b
neiljohnson <mr.neil.j@gmail.com>
2017-02-05 15:01:53 -0800

    Added files needed for the program to successfully run (though blank).

---
+++ BankAccount.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Neil Johnson on 2/5/17.
+//
+
+#include "BankAccount.hpp"
---
+++ BankAccount.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Neil Johnson on 2/5/17.
+//
+
+#ifndef PROJ_5B_BANKACCOUNT_HPP
+#define PROJ_5B_BANKACCOUNT_HPP
+
+
+class BankAccount {
+
+};
+
+
+#endif //PROJ_5B_BANKACCOUNT_HPP
