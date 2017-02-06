b4944dd72b8852f6491100bc647feba523068fad
Neil Johnson <mr.neil.j@gmail.com>
2017-02-06 15:04:51 -0800

    Added files needed for the program to successfully run (though blank). updated .hpp file with comments and further implemented classes. Added in all class function declarations (haven't fully defined each and every one of them) Ran through code analysis and fixed any outstanding errors in the code (need to re-comment to match standards) Added Description headers to each of the three files (main, BankAccount.hpp / .cpp Added Description to the .cpp file for better understanding on each function
    Added test examples to ensure application is running properly

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
  * examples in the guidelines for the Assignment.
  *************************************************************/
 
+#include <iostream>
 #include "BankAccount.hpp"
 
 
@@ -20,5 +21,21 @@ int main() {
     account1.deposit(500.00);
     double finalBalance = account1.getCustomerBalance();
 
+    // Test example
+    BankAccount accountTest("Johnny Appleseed", "A1234", 2000.32);
+
+    std::cout << "Bank Account Information:" << std:: endl;
+    std::cout << "=================================" << std::endl;
+    std::cout << "Name:    " << accountTest.getCustomerName() << std::endl;
+    std::cout << "ID:      " << accountTest.getCustomerID() << std::endl;
+    std::cout << "Balance: " << accountTest.getCustomerBalance() << std::endl;
+    std::cout << "---------------------------------\n" << std::endl;
+
+    // Adjust Balance values for accountTest
+    std::cout << "...Withdrawing 500 Dollars........" << std::endl;
+    accountTest.withdraw(500);
+    std::cout << "Updated Balance: " << accountTest.getCustomerBalance() << std::endl;
+
+    std::cout << "\n...Depositing 1000 Dollars........" << std::endl;
     return 0;
 }
